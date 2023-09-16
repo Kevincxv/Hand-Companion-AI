c1c4861d645df5a4e16a03f8c343d661e185d20f
NassG1214 <giancarlonassi@gmail.com>
2023-09-16 03:23:24 -0400

    Update
    Added main page for js and csss

--- Front-End/my-app/src/App.test.js
+++ Front-End/my-app/src/App.test.js
@@ -1,4 +1,4 @@
-cdimport { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
