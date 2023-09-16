406d35e48eea49778b4bdec65e23b187ffac3765
Kevin <Kevincxv@outlook.com>
2023-09-16 16:01:32 -0400

    Completely updated
    This is very close to the finally version but still going to implement more features

--- AI-Model/AI-Model/test.py
+++ AI-Model/AI-Model/test.py
@@ -1,9 +1,12 @@
+from flask import Flask, render_template, jsonify, request
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
 import time
+from gtts import gTTS
+import os
 
 last_error_displayed = {
     "camera_error": 0,
@@ -22,20 +25,42 @@ def can_display_error(error_key):
     return False
 
 
-s = ""
-my_message = []
-
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
+detector = HandDetector(maxHands=2)
 classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
 
 offset = 20
 imgSize = 300
 
+last_print_time = 0
 counter = 0
+last_capture_time = 0
+capture_interval = 4
 
 labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
           "X", "Y", "Z"]
+captured_chars = []
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_captured_chars', methods=['GET'])
+def get_captured_chars():
+    return jsonify({"captured_chars": captured_chars})
+
+
+@app.route('/get_audio', methods=['GET'])
+def get_audio():
+    return send_file("captured_audio.mp3", as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 while True:
     try:
@@ -47,6 +72,10 @@ while True:
 
         imgOutput = img.copy()
         hands, img = detector.findHands(img)
+
+        if len(hands) == 2:
+            break
+
         if hands:
             hand = hands[0]
             x, y, w, h = hand['bbox']
@@ -72,27 +101,21 @@ while True:
                 imgWhite[hGap:hCal + hGap, :] = imgResize
                 prediction, index = classifier.getPrediction(imgWhite, draw=False)
 
-            last_print_time = 0
             current_time = time.time()
-            if current_time - last_print_time >= 2:
-                my_message.append(labels[index])
-                print(f"{labels[index]}")
-                last_print_time = current_time
+            if current_time - last_capture_time >= capture_interval:
+                captured_char = labels[index]
+                if not captured_chars or (captured_chars and captured_chars[-1] != captured_char):
+                    captured_chars.append(captured_char)
+                    print(f"{captured_chars}")
+                    last_capture_time = current_time
 
-            cv2.rectangle(imgOutput, (x - offset, y - offset - 50), (x - offset + 90, y - offset - 50 + 50), (0, 245, 0), cv2.FILLED)
+            cv2.rectangle(imgOutput, (x - offset, y - offset - 50), (x - offset + 90, y - offset - 50 + 50),
+                          (0, 245, 0), cv2.FILLED)
             cv2.putText(imgOutput, labels[index], (x, y - 25), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
             cv2.rectangle(imgOutput, (x - offset, y - offset), (x + w + offset, y + h + offset), (0, 245, 0), 4)
 
             cv2.imshow("ImageCrop", imgCrop)
             cv2.imshow("ImageWhite", imgWhite)
-
-            if len(hands) > 1:
-                for x in my_message:
-                    s += x
-                print(s)
-                s = ""
-                my_message.clear()
-
 
         cv2.imshow("Image", imgOutput)
         cv2.waitKey(1)
@@ -106,3 +129,9 @@ while True:
     if not hands:
         if can_display_error("no_hand_detected_error"):
             print("No hand detected. Please place your hand inside the frame.")
+
+if captured_chars:
+    tts_text = ' '.join(captured_chars)
+    tts = gTTS(text=tts_text, lang='en')
+    tts.save("captured_audio.mp3")
+    os.system("afplay captured_audio.mp3")
