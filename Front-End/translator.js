b871340d5412ff3644559726bcf35bb6ca88ea56
NassG1214 <giancarlonassi@gmail.com>
2023-09-16 16:35:31 -0400

    Update Translator
    Translator for html, css, js

---
+++ Front-End/translator.js
@@ -0,0 +1,11 @@
+const videoElement = document.getElementById('camera');
+const webcam = new Webcam(videoElement, 'user');
+
+// Start the webcam automatically when the page loads
+webcam.start()
+    .then(result => {
+        console.log("webcam started");
+    })
+    .catch(err => {
+        console.log(err);
+    });
